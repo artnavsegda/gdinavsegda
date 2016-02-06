f25a503c4a9c54c5cf5ebfa15370fb08fc301cb9
artnavsegda <artnavsegda@gmail.com>
2016-02-06 15:05:59 +0300

    modbus

--- antrack/antrack.c
+++ antrack/antrack.c
@@ -19,28 +19,29 @@ int main(int argc, char *argv[])
 	}
 
 	mb = modbus_new_tcp(argv[1], 502);
-	if (modbus_connect(mb) == -1)
-	{
-		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-		modbus_free(mb);
-		return -1;
-	}
-
 	while (1)
 	{
+		while (modbus_connect(mb) == -1)
+		{
+			fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+			//modbus_free(mb);
+			//return -1;
+		}
+
+
 		/* Read 5 registers from the address 10 */
-		rc = modbus_read_registers(mb, 10, 40, tab_reg);
+		rc = modbus_read_registers(mb, 0, 6, tab_reg);
 		if (rc == -1) {
 			fprintf(stderr, "%s\n", modbus_strerror(errno));
 			return -1;
 		}
 
-		for (i = 0; i < rc; i = i + 2) {
-			printf("reg[%d]=%f (0x%X)\n", i, modbus_get_float(&tab_reg[i]), tab_reg[i]);
+		for (i = 0; i < rc; i = i++) {
+			printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
 		}
+		modbus_close(mb);
 		Sleep(1000);
 	}
 
-	modbus_close(mb);
 	modbus_free(mb);
 }
