4b6ea7ebe0d59f66aba2e004ea46dfbd6405778f
Art Navsegda <artnavsegda@gmail.com>
2016-11-26 19:28:12 +0300

    lel

--- serial/serial.c
+++ serial/serial.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main()
 {
 	printf("hello world\n");
+	CreateFile("COM4", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
     return 0;
 }
-
