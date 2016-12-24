c4f23e23692ee83e73885341cfff7799d3c7450e
Art Navsegda <artnavsegda@gmail.com>
2016-12-24 09:53:04 +0300

    serialrecord

--- serialrecord/serialrecord.c
+++ serialrecord/serialrecord.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
 
+char szFile[260] = "record.txt";
+
+OPENFILENAME ofn = {
+	.lStructSize = sizeof(ofn),
+	.lpstrFile = szFile,
+	.nMaxFile = sizeof(szFile),
+	.lpstrFilter = "Text\0*.TXT\0All\0*.*\0",
+	.nFilterIndex = 1,
+	.lpstrFileTitle = NULL,
+	.nMaxFileTitle = 0,
+	.lpstrInitialDir = NULL,
+	.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR
+};
+
 void oshibka(char *oshibkaname)
 {
 	LPVOID lpMsgBuf;
@@ -12,21 +26,26 @@ void oshibka(char *oshibkaname)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc != 2)
 	{
-		printf("name serial port and file name\n");
+		printf("name serial port\n");
 		exit(1);
 	}
 
 	HANDLE serial = CreateFile(argv[1], GENERIC_READ, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 	if (serial == INVALID_HANDLE_VALUE)
 		oshibka("cannot open serial");
-	HANDLE outputfile = CreateFile(argv[2], GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
-	if (serial == INVALID_HANDLE_VALUE)
+
+	if (GetSaveFileName(&ofn) != TRUE)
+		exit(1);
+	HANDLE outputfile = CreateFile(ofn.lpstrFile, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	if (outputfile == INVALID_HANDLE_VALUE)
 		oshibka("cannot create record file");
-	DCB serialparams = { .BaudRate = CBR_9600,.ByteSize = 8,.StopBits = ONESTOPBIT,.Parity = NOPARITY };
+
+	DCB serialparams = { .BaudRate = CBR_115200,.ByteSize = 8,.StopBits = ONESTOPBIT,.Parity = NOPARITY };
 	if (!SetCommState(serial, &serialparams))
 		oshibka("cannot set serial params");
+
 	COMMTIMEOUTS timeouts = {
 		.ReadIntervalTimeout = 50,
 		.ReadTotalTimeoutConstant = 50,
@@ -44,7 +63,10 @@ int main(int argc, char *argv[])
 	{
 		ReadFile(serial, buffer, 1, &numread, NULL);
 		if (numread == 1)
+		{
 			WriteFile(outputfile, buffer, 1, &numread, NULL);
+			putchar(buffer[0]);
+		}
 	}
 
 	CloseHandle(serial);
