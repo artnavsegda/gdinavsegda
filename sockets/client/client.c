3efdcb337a4861dd0dfe1c59591bd2e17123e290
Art Navsegda <artnavsegda@gmail.com>
2016-12-28 00:41:11 +0300

    sockets

---
+++ sockets/client/client.c
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <winsock.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Wsock32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	return 0;
+}
