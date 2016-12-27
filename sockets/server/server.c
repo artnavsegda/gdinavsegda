701d6c7fab78dec4217e91f148506abd83d7174b
Art Navsegda <artnavsegda@gmail.com>
2016-12-28 00:52:18 +0300

    server

--- sockets/server/server.c
+++ sockets/server/server.c
@@ -16,5 +16,17 @@ int main()
 		printf("WSAStartup failed: %d\n", iResult);
 		return 1;
 	}
+
+	SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (sock == INVALID_SOCKET)
+	{
+		printf("socket error\n");
+		WSACleanup();
+		exit(0);
+	}
+
+	//if (bind(sock,&server, sizeof(server)))
+
 	return 0;
 }
