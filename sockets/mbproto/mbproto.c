2ac9ebe5eb3944bb8a5d0ac655ba827976b93634
Art Navsegda <artnavsegda@gmail.com>
2017-01-01 12:58:45 +0300

    bone

--- sockets/mbproto/mbproto.c
+++ sockets/mbproto/mbproto.c
@@ -13,8 +13,73 @@ void oshibka(char *oshibkaname)
 	exit(1);
 }
 
-int main()
+struct tcpframestruct {
+	unsigned short tsid;
+	unsigned short protoid;
+	unsigned short length;
+};
+
+struct askreadregstruct {
+	unsigned short firstreg;
+	unsigned short regnumber;
+};
+
+struct writeregstruct {
+	unsigned short regaddress;
+	unsigned short regvalue;
+};
+
+struct reqreadbitstruct {
+	unsigned char bytestofollow;
+	unsigned char coils[256];
+};
+
+struct reqreadwordstruct {
+	unsigned char bytestofollow;
+	unsigned short registers[127];
+};
+
+union pdudataunion {
+	struct askreadregstruct askreadregs;
+	struct reqreadbitstruct reqreadcoils;
+	struct reqreadwordstruct reqreadholdings;
+	struct writeregstruct writereg;
+	unsigned char bytes[254];
+	unsigned short words[127];
+};
+
+struct pduframestruct {
+	unsigned char unitid;
+	unsigned char fncode;
+	union pdudataunion data;
+};
+
+struct mbframestruct {
+	unsigned short tsid;
+	unsigned short protoid;
+	unsigned short length;
+	struct pduframestruct pdu;
+};
+
+struct mbframestruct mbframe = {
+	.tsid = 0x0100,
+	.protoid = 0x0000,
+	.length = 0x0600,
+	.pdu = {
+	.unitid = 50
+	}
+};
+
+struct mbframestruct askframe;
+
+int main(int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		printf("name function code and both values\n");
+		return 1;
+	}
+
 	WSADATA wsaData;
 	int iResult;
 
@@ -43,9 +108,9 @@ int main()
 	}
 
 	struct sockaddr_in client = {
-		.sin_addr.s_addr = inet_addr("192.168.1.150"),
+		.sin_addr.s_addr = inet_addr("127.0.0.1"),
 		.sin_family = AF_INET,
-		.sin_port = htons(502)
+		.sin_port = htons(1100)
 	};
 
 	if (connect(sock, (struct sockaddr *)&client, sizeof(client)) == SOCKET_ERROR)
@@ -59,9 +124,15 @@ int main()
 		printf("connect ok\n");
 	}
 
-	int numwrite = send(sock, "hello", 6, 0);
+	sscanf(argv[1], "%hhu", &mbframe.pdu.fncode);
+	sscanf(argv[2], "%hu", &mbframe.pdu.data.words[0]);
+	mbframe.pdu.data.words[0] = htons(mbframe.pdu.data.words[0]);
+	sscanf(argv[3], "%hu", &mbframe.pdu.data.words[1]);
+	mbframe.pdu.data.words[1] = htons(mbframe.pdu.data.words[1]);
 
-	if (numwrite = -1)
+	int numwrite = send(sock, &mbframe, 12, 0);
+
+	if (numwrite = SOCKET_ERROR)
 	{
 		oshibka("send");
 	}
@@ -70,7 +141,64 @@ int main()
 		printf("send %d bytes ok\n", numwrite);
 	}
 
-	shutdown(sock, 2);
+	int numread = recv(sock, &askframe, 100, 0);
+	if (numwrite == SOCKET_ERROR)
+	{
+		oshibka("1 recv");
+	}
+	else
+	{
+		printf("1 recv %d bytes\n", numread);
+		printf("TS id: %d\n", ntohs(askframe.tsid));
+		printf("Protocol id: %d\n", ntohs(askframe.protoid));
+		printf("Length: %d\n", ntohs(askframe.length));
+	}
+
+	numread = recv(sock, &askframe.pdu, ntohs(askframe.length), 0);
+	if (numwrite == SOCKET_ERROR)
+	{
+		oshibka("2 recv");
+	}
+	else
+	{
+		printf("2 recv %d bytes\n", numread);
+		printf("Unit id: %d\n", askframe.pdu.unitid);
+		printf("Function code: %d\n", askframe.pdu.fncode);
+		for (int i = 0; i < numread - 2; i++)
+			printf("0x%02x ", askframe.pdu.data.bytes[i]);
+		printf("\n");
+		switch (askframe.pdu.fncode)
+		{
+		case 1:
+		case 2:
+			printf("number of bytes: %d\n", askframe.pdu.data.reqreadcoils.bytestofollow);
+			for (int i = 0; i < askframe.pdu.data.reqreadcoils.bytestofollow; i++)
+				printf("0x%02hhX ", askframe.pdu.data.reqreadcoils.coils[i]);
+			printf("\n");
+			break;
+		case 3:
+		case 4:
+			printf("number of registers: %d\n", askframe.pdu.data.reqreadholdings.bytestofollow / 2);
+			for (int i = 0; i < askframe.pdu.data.reqreadholdings.bytestofollow / 2; i++)
+				printf("0x%04hX ", askframe.pdu.data.reqreadholdings.registers[i]);
+			printf("\n");
+			break;
+		case 5:
+		case 6:
+			printf("register number %u\n", ntohs(askframe.pdu.data.writereg.regaddress));
+			printf("register value %u\n", ntohs(askframe.pdu.data.writereg.regvalue));
+			break;
+		}
+	}
+
+	if (shutdown(sock, 2) == SOCKET_ERROR)
+	{
+		oshibka("send");
+	}
+	else
+	{
+		printf("shutdown ok\n");
+	}
 	closesocket(sock);
 
 	return 0;
