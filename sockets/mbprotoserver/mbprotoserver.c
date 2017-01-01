554360e8fe3f7477048bf76d4d82004bf42a0b48
Art Navsegda <artnavsegda@gmail.com>
2017-01-01 13:18:33 +0300

    fdg

--- sockets/mbprotoserver/mbprotoserver.c
+++ sockets/mbprotoserver/mbprotoserver.c
@@ -13,6 +13,45 @@ void oshibka(char *oshibkaname)
 	exit(1);
 }
 
+struct askreadregstruct {
+	unsigned short firstreg;
+	unsigned short regnumber;
+};
+
+struct reqreadcoilsstruct {
+	unsigned char bytestofollow;
+	unsigned char coils[256];
+};
+
+struct reqreadwordstruct {
+	unsigned char bytestofollow;
+	unsigned short registers[127];
+};
+
+union pdudataunion {
+	struct askreadregstruct askreadregs;
+	struct reqreadcoilsstruct reqreadcoils;
+	struct reqreadwordstruct reqreadholdings;
+	unsigned short words[127];
+	unsigned char bytes[254];
+};
+
+struct pduframestruct {
+	unsigned char unitid;
+	unsigned char fncode;
+	union pdudataunion data;
+	//	unsigned short data[256];
+};
+
+struct mbframestruct {
+	unsigned short tsid;
+	unsigned short protoid;
+	unsigned short length;
+	struct pduframestruct pdu;
+};
+
+struct mbframestruct askmbframe;
+
 int main()
 {
 	int buf[100];
@@ -74,7 +113,6 @@ int main()
 		printf("listen ok\n");
 	}
 
-
 	while (1)
 	{
 		SOCKET msgsock = accept(sock, NULL, NULL);
@@ -89,8 +127,65 @@ int main()
 		{
 			printf("accept ok\n");
 		}
-		while (recv(msgsock, buf, 100, 0) != SOCKET_ERROR)
-			;
+
+		int numread = recv(msgsock, &askmbframe, 6, 0);
+		if (numread == SOCKET_ERROR)
+		{
+			oshibka("1 recv");
+		}
+		else
+		{
+			printf("1 recv %d bytes\n", numread);
+			printf("TS id: %d\n", ntohs(askmbframe.tsid));
+			printf("Protocol id: %d\n", ntohs(askmbframe.protoid));
+			printf("Length: %d\n", ntohs(askmbframe.length));
+		}
+
+		numread = recv(msgsock, &askmbframe.pdu, ntohs(askmbframe.length), 0);
+		if (numread == SOCKET_ERROR)
+		{
+			oshibka("2 recv");
+		}
+		else
+		{
+			printf("2 recv %d bytes\n", numread);
+			printf("Unit id: %d\n", askmbframe.pdu.unitid);
+			printf("Function code: %d\n", askmbframe.pdu.fncode);
+			for (int i = 0; i<(numread - 2) / 2; i++)
+				printf("%u ", ntohs(askmbframe.pdu.data.words[i]));
+			printf("\n");
+		}
+
+		switch (askmbframe.pdu.fncode)
+		{
+		case 1:
+		case 2:
+			askmbframe.pdu.data.reqreadcoils.bytestofollow = ntohs(askmbframe.pdu.data.askreadregs.regnumber) / 8;
+			if ((ntohs(askmbframe.pdu.data.askreadregs.regnumber) % 8)>0)
+				askmbframe.pdu.data.reqreadcoils.bytestofollow++;
+			askmbframe.length = htons(askmbframe.pdu.data.reqreadcoils.bytestofollow + 3);
+			break;
+		case 3:
+		case 4:
+			printf("numer of registers requested %d\n", ntohs(askmbframe.pdu.data.askreadregs.regnumber));
+			askmbframe.pdu.data.reqreadholdings.bytestofollow = ntohs(askmbframe.pdu.data.askreadregs.regnumber) * 2;
+			askmbframe.length = htons(askmbframe.pdu.data.reqreadholdings.bytestofollow + 3);
+			break;
+		}
+
+		int replylength = ntohs(askmbframe.length) + 6;
+		printf("replylength %d\n", replylength);
+
+		int numwrite = send(msgsock, &askmbframe, replylength, 0);
+		if (numwrite = SOCKET_ERROR)
+		{
+			oshibka("send");
+		}
+		else
+		{
+			printf("send %d bytes ok\n", numwrite);
+		}
+
 		if (shutdown(msgsock, 2) == SOCKET_ERROR)
 		{
 			oshibka("shutdown");
