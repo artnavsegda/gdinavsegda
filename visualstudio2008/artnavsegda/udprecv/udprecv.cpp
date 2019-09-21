81600c79ba6db96759ad2a14bc557bc9cf34a747
Art Navsegda <artnavsegda@gmail.com>
2019-09-22 00:00:29 +0300

    ya

---
+++ visualstudio2008/artnavsegda/udprecv/udprecv.cpp
@@ -0,0 +1,96 @@
+#include <windows.h>
+#include <winsock.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Wsock32.lib")
+
+void oshibka(char *oshibkaname)
+{
+	LPVOID lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("%s: %s\n", oshibkaname, lpMsgBuf);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+int main()
+{
+	unsigned char buf[100];
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	else
+	{
+		printf("WSAStartup ok\n");
+	}
+
+	SOCKET sock = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+
+	if (sock == INVALID_SOCKET)
+	{
+		oshibka("socket");
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+
+	struct sockaddr_in server = {};
+	server.sin_family = AF_INET;
+	server.sin_port = htons(1100);
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	struct sockaddr_in other;
+	int slen = sizeof(other);
+
+	if (bind(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		oshibka("bind");
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		printf("bind ok\n");
+	}
+
+	int numread = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&other, &slen);
+
+	if (numread == SOCKET_ERROR)
+	{
+		oshibka("recvfrom");
+	}
+	else
+	{
+		printf("recv %d bytes\n", numread);
+		for (int i = 0; i<numread; i++)
+		{
+			printf("0x%02X, ", buf[i]);
+		}
+		printf("\n");
+	}
+
+	if (shutdown(sock, 2) == SOCKET_ERROR)
+	{
+		oshibka("shutdown");
+	}
+	else
+	{
+		printf("shutdown ok\n");
+	}
+	closesocket(sock);
+
+	getchar();
+
+	return 0;
+}
