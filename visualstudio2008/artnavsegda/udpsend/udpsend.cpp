bf39785ec8b0dc4f6eddb6e20352b3f008d850cb
Art Navsegda <artnavsegda@gmail.com>
2019-09-21 23:30:43 +0300

    Update udpsend.cpp

--- visualstudio2008/artnavsegda/udpsend/udpsend.cpp
+++ visualstudio2008/artnavsegda/udpsend/udpsend.cpp
@@ -1,11 +1,77 @@
-// udpsend.cpp : Defines the entry point for the console application.
-//
+#include <windows.h>
+#include <winsock.h>
+#include <stdio.h>
 
-#include "stdafx.h"
+#pragma comment(lib, "Wsock32.lib")
 
-
-int _tmain(int argc, _TCHAR* argv[])
+void oshibka(char *oshibkaname)
 {
-	return 0;
+	LPVOID lpMsgBuf;
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpMsgBuf, 0, NULL);
+	printf("%s: %s\n", oshibkaname, lpMsgBuf);
+	LocalFree(lpMsgBuf);
+	exit(1);
 }
 
+char package[] = "hello world\n";
+
+int main()
+{
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+	else
+	{
+		printf("WSAStartup ok\n");
+	}
+
+	SOCKET sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET)
+	{
+		oshibka("socket");
+		WSACleanup();
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+
+	struct sockaddr_in other = {
+		.sin_addr.s_addr = inet_addr("127.0.0.1"),
+		.sin_family = AF_INET,
+		.sin_port = htons(1100)
+	};
+
+	int slen = sizeof(other);
+
+	int numwrite = sendto(sock, package, sizeof(package), 0, (struct sockaddr *)&other, slen);
+
+	if (numwrite == SOCKET_ERROR)
+	{
+		oshibka("send");
+	}
+	else
+	{
+		printf("send %d bytes ok\n", numwrite);
+	}
+
+	if (shutdown(sock, 2) == SOCKET_ERROR)
+	{
+		oshibka("shutdown");
+	}
+	else
+	{
+		printf("shutdown ok\n");
+	}
+	closesocket(sock);
+
+	return 0;
+}
