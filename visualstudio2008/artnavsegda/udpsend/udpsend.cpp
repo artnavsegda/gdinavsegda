f8d05b6738155d73227af65bcbbd7910e5b289b1
Art Navsegda <artnavsegda@gmail.com>
2019-12-19 17:43:03 +0300

    y

--- visualstudio2008/artnavsegda/udpsend/udpsend.cpp
+++ visualstudio2008/artnavsegda/udpsend/udpsend.cpp
@@ -13,10 +13,30 @@ void oshibka(char *oshibkaname)
 	exit(1);
 }
 
+struct sockaddr* resolve(char* hostname, char* port)
+{
+	static struct sockaddr_in client;
+	struct hostent* host = gethostbyname(hostname);
+
+	if (host != NULL)
+		client.sin_addr.s_addr = *(u_long*)host->h_addr_list[0];
+	else
+		client.sin_addr.s_addr = inet_addr(hostname);
+	client.sin_family = AF_INET;
+	client.sin_port = htons(atoi(port));
+	return (struct sockaddr*) &client;
+}
+
 char package[] = "hello world\n";
 
 int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		puts("usage: tcpsend <ipaddr> <port>");
+		return 0;
+	}
+
 	WSADATA wsaData;
 	int iResult;
 
@@ -44,14 +64,7 @@ int main(int argc, char* argv[])
 		printf("socket ok\n");
 	}
 
-	struct sockaddr_in other = {};
-	other.sin_addr.s_addr = inet_addr("127.0.0.1");
-	other.sin_family = AF_INET;
-	other.sin_port = htons(1100);
-
-	int slen = sizeof(other);
-
-	int numwrite = sendto(sock, package, sizeof(package), 0, (struct sockaddr *)&other, slen);
+	int numwrite = sendto(sock, package, sizeof(package), 0, resolve(argv[1], argv[2]), sizeof(sockaddr_in));
 
 	if (numwrite == SOCKET_ERROR)
 	{
