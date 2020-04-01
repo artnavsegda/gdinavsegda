7dc59db0ad874f8e6a892b9d4ea0a48a1982081a
Art Navsegda <artnavsegda@gmail.com>
2020-04-01 03:56:29 +0300

    mfc

---
+++ visualstudio2008/artnavsegda/mfchello/mfchello.h
@@ -0,0 +1,19 @@
+ 	#include <afxwin.h>
+ 	// Класс основного окна приложения
+ 	class CMainWin: public CFrameWnd {
+ 	public:
+ 	CMainWin();
+ 	// Декларирование карты сообщений
+ 	DECLARE_MESSAGE_MAP()
+ 	};
+
+ 	// Класс приложения. Должен существовать только
+ 	// один экземпляр этого класса.
+ 
+ 	// Член-функция InitInstance() вызывается при запуске
+ 	// приложения.
+ 
+ 	class CApp: public CWinApp {
+ 	public:
+ 	BOOL InitInstance();
+ 	};
