e57d93c4cabafe2ba67cd4ce59fbcbe3e338463e
Art Navsegda <artnavsegda@gmail.com>
2020-10-22 14:58:06 +0300

    Update main.cpp

--- visualstudio2008/artnavsegda/cipcontrol/main.cpp
+++ visualstudio2008/artnavsegda/cipcontrol/main.cpp
@@ -4,6 +4,8 @@
 
 #pragma comment(lib, "Wsock32.lib")
 
+#define CIP_PORT 41794
+
 void oshibka(char *oshibkaname)
 {
 	LPVOID lpMsgBuf;
@@ -13,7 +15,7 @@ void oshibka(char *oshibkaname)
 	exit(1);
 }
 
-struct sockaddr* resolve(char* hostname, char* port)
+struct sockaddr* resolve(char* hostname)
 {
 	static struct sockaddr_in client;
 	struct hostent* host = gethostbyname(hostname);
@@ -23,15 +25,15 @@ struct sockaddr* resolve(char* hostname, char* port)
 	else
 		client.sin_addr.s_addr = inet_addr(hostname);
 	client.sin_family = AF_INET;
-	client.sin_port = htons(atoi(port));
+	client.sin_port = htons(CIP_PORT);
 	return (struct sockaddr*) &client;
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	if (argc != 2)
 	{
-		puts("usage: tcprecv <ipaddr> <port>");
+		puts("usage: cipcontrol <ipaddr>");
 		return 0;
 	}
 
@@ -62,7 +64,7 @@ int main(int argc, char* argv[])
 		printf("socket ok\n");
 	}
 
-	if (connect(sock, resolve(argv[1], argv[2]), sizeof(struct sockaddr_in)) == SOCKET_ERROR)
+	if (connect(sock, resolve(argv[1]), sizeof(struct sockaddr_in)) == SOCKET_ERROR)
 	{
 		oshibka("connect");
 		closesocket(sock);
