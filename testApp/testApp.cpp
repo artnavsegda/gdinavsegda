3a48171ebe9edbee2b57bec4211c339b981d5c0e
Art Navsegda <artnavsegda@gmail.com>
2024-08-16 01:54:26 +0300

    Update testApp.cpp

--- testApp/testApp.cpp
+++ testApp/testApp.cpp
@@ -1,10 +1,34 @@
 #include <windows.h>
 #include <stdio.h>
 
+typedef int (*card_authorize9)(char* track2, struct auth_answer9* auth_answer);
+
 int main()
 {
     printf("start\n");
-    int msgboxID = MessageBox(
+
+    HMODULE pilotNt = LoadLibraryA("pilot_nt.dll");
+
+    if (!pilotNt)
+    {
+        printf("pilot_nt.dll not found\n");
+        return -1;
+    }
+
+    try {
+        card_authorize9 function = (card_authorize9)GetProcAddress(pilotNt, "card_authorize9");
+        if (!function)
+        {
+            printf("pilot_nt.dll not found\n");
+            return -2;
+        }
+    }
+    catch (...)
+    {
+        FreeLibrary(pilotNt);
+    }
+
+    /*int msgboxID = MessageBox(
         NULL,
         L"Sdelat oplaty ?",
         L"Okoshko",
@@ -15,7 +39,7 @@ int main()
     {
         return 321;
     }
-    printf("stop\n");
+    printf("stop\n");*/
 
     //printf("starting to sleep...\n");
     //Sleep(3000); // Sleep three seconds
